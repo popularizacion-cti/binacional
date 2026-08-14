b81ee9b0f4442d28bd89c4e8a23c61d3d8aecc22
popularizacion-cti <popularizacion@concytec.gob.pe>
2026-08-14 17:20:12 -0500

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -116,7 +116,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 // CONTADOR PARA EL BANNER DE ALERTA
-const targetDate = new Date("August 14, 2026 23:59:59").getTime();
+const targetDate = new Date("August 17, 2026 12:00:00").getTime();
 
 setInterval(() => {
     const now = new Date().getTime();
